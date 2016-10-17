4105713be264b53eab73ea83a1dd4541fe267b2e
vignesh <vigneshvs05@gmail.com>
2016-10-17 10:49:15 -0700

    move views to view folder

--- app/app.checkinform.component.ts
+++ app/app.checkinform.component.ts
@@ -3,43 +3,31 @@ import { Component,Directive,OnInit, ElementRef} from '@angular/core';
 import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { CheckIn } from './app.checkinform.interface';
 
-
-
 @Directive({
     selector: 'input[formControlName=bookingid]',
     host: {
         '(input)': 'ref.nativeElement.value=$event.target.value.toUpperCase()',
     }
-
 })
 export class UpperCaseText {
     constructor(private ref: ElementRef) {
     }
 }
-
-
 @Component({    
     selector: 'my-app',   
-    templateUrl:'./app/app.checkin.component.html',
+    templateUrl:'./app/view/app.checkin.component.html',
     styleUrls:['./app/css/app.checkinform.styles.css']
 
 })
 export class CheckInFormComponent implements OnInit{
     public checkinForm: FormGroup;
     public submitted: boolean;
-    constructor(private formBuilder: FormBuilder){}
-
-  
+    constructor(private formBuilder: FormBuilder){}  
 ngOnInit() {
 
     this.checkinForm =  this.formBuilder.group({
         'bookingcode': [null, <any>Validators.required],
         'familyname' :[null , <any>Validators.required]
-      
-        
     });
-    
-
 }
-
 }
